57117cb1bb084112c9a96949bae8d9da10164812
Brian Abbott <brianabbott@Zipcoder25s-MacBook-Pro.local>
2016-06-15 17:20:54 -0400

    First commit

---
+++ src/main/java/com/oncheck/OnCheckMiddleWareApplication.java
@@ -0,0 +1,12 @@
+package com.oncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnCheckMiddleWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnCheckMiddleWareApplication.class, args);
+	}
+}
