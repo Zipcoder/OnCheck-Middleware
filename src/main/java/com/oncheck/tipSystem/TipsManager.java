3c1cd234c9b068bdc7b4d4dbbc4a9ebb01846f2c
Wesley Traute <wesleytraute@Zipcoder20s-MBP.home>
2016-06-16 21:42:01 -0400

    Added TipController & map, added submitTip and commented out for dependencies

--- src/main/java/com/oncheck/tipSystem/TipsManager.java
+++ src/main/java/com/oncheck/tipSystem/TipsManager.java
@@ -1,23 +1,44 @@
 package com.oncheck.tipSystem;
 
-import java.sql.*;
 
-public class TipsManager {
+import org.springframework.core.io.FileSystemResource;
 
-    TipFactory tipFactory = new TipFactory();
-    String myUrl = "localhost/tips";
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+class TipsManager {
+
+    private TipFactory tipFactory = new TipFactory();
+    private String myUrl = "localhost/tips";
 
     /**
      * SENDING TIP TO OFFICIAL PARTY
      */
-    public void submitTip() {
+    private void submitTip(Tip tip) {
+
+//        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+//        sender.setHost("mail.foodoncheck.io");
+//
+//        MimeMessage message = sender.createMimeMessage();
+//
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//        helper.setTo("someofficialperson@dhss.gov");
+//
+//        helper.setText(tip.getMessage());
+//
+//        FileSystemResource file = new FileSystemResource(new File(tip.getEvidence()));
+//        helper.addAttachment("Evidence.jpg", file);
+//
+//        sender.send(message);
 
     }
 
     /**
      * ADDING NEW TIP TO THE DATABASE
      */
-    public void addTip(String message, String evidence) {
+    void addTip(String message, String evidence) {
         try {
 
             Connection dbConnection = DriverManager.getConnection(myUrl, "root", "");
@@ -36,10 +57,13 @@ public class TipsManager {
 
             dbConnection.close();
 
+            //submitTip(tip);
+
         }
         catch (Exception e) {
 
-
+            System.err.println("Encountered an exception");
+            e.printStackTrace();
 
         }
     }
