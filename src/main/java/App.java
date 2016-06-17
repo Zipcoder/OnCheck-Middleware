15a0bb8bfc99571f5484c68673123f62b28e1c54
Veronica Crowe <vncrowe@gmail.com>
2016-06-17 17:34:09 -0400

    first commit to app

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by veronicacrowe on 6/16/16.
+ */
+public class App {
+    AccountManager accountManger = new AccountManager();
+    RestaurantManager restaurantManager = new RestaurantManager();
+    TipManager tipManager = new TipManager();
+
+    public void start(){
+
+    }
+
+}
