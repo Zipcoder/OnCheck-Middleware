81ca1f888f8b53e58d0b6640e706a2796d693308
Wesley Traute <wesleytraute@Zipcoder20s-MacBook-Pro.local>
2016-07-08 16:08:07 -0400

    Commented out some stuff

--- src/main/java/com/oncheck/Controller/UserManager.java
+++ src/main/java/com/oncheck/Controller/UserManager.java
@@ -1,59 +1,68 @@
-package com.oncheck.Controller;
-
-import com.oncheck.Repository.UserRepository;
-import com.oncheck.Restaurant;
-import com.oncheck.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-public class UserManager {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    @ResponseBody
-    public boolean accountLoggedIn(){
-        return true;
-    }
-
-
-    @RequestMapping(value = "/account/read/{userID}", method = RequestMethod.GET)
-    @ResponseBody
-    public User readAccount(@PathVariable Long userID) {
-        return userRepository.get(userID);
-    }
-
-//    @RequestMapping
-//    @ResponseBody(value = "account/update/{userID}", method = RequestMethod.PUT)
-//    public void updateAccount(@PathVariable) {
-
-//        PUT info for columns in userID
-
-//    }
-
-    @RequestMapping(value = "account/delete/{userID}", method = RequestMethod.DELETE)
-    @ResponseBody
-    public void deleteAccount(@PathVariable Long userID) {
-        userRepository.deleteAccount(userID);
-
-    }
-
-    @RequestMapping(value = "account/create/{userID}", method = RequestMethod.POST)
-    @ResponseBody
-    public User enterUsername(@PathVariable Long userID){
-        return userRepository.save(new User());
-    }
-
-
-//    public Restaurant addToMyEats(Restaurant restaurantID){
-//        myEats.add(restaurantID);
-//        return restaurantID;
+//package com.oncheck.Controller;
+//
+//import com.oncheck.Repository.UserRepository;
+//import com.oncheck.Restaurant;
+//import com.oncheck.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//public class UserManager {
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    @ResponseBody
+//    public boolean accountLoggedIn(){
+//        return true;
 //    }
 //
-//    public boolean removeFromMyEats(Restaurant restaurantID){
-//        myEats.remove(restaurantID);
-//        return false;
+//
+//    @RequestMapping(value = "/account/read/{userID}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public User readAccount(@PathVariable Long userID) {
+//        return userRepository.get(userID);
 //    }
-}
+//
+////    @RequestMapping
+////    @ResponseBody(value = "account/update/{userID}", method = RequestMethod.PUT)
+////    public void updateAccount(@PathVariable) {
+//
+////        PUT info for columns in userID
+//
+////    }
+//
+//    @RequestMapping(value = "/account/delete/{userID}", method = RequestMethod.DELETE)
+//    @ResponseBody
+//    public void deleteAccount(@PathVariable Long userID) {
+//        userRepository.deleteAccount(userID);
+//
+//    }
+//
+//    @RequestMapping(value = "/account/create/{userID}", method = RequestMethod.POST)
+//    @ResponseBody
+//    public User enterUsername(@PathVariable Long userID){
+//        return userRepository.save(new User());
+//    }
+//
+//
+////    public Restaurant addToMyEats(Restaurant restaurantID){
+////        myEats.add(restaurantID);
+////        return restaurantID;
+////    }
+////
+////    public boolean removeFromMyEats(Restaurant restaurantID){
+////        myEats.remove(restaurantID);
+////        return false;
+////    }
+//
+//
+//    public UserRepository getUserRepository() {
+//        return userRepository;
+//    }
+//
+//    public void setUserRepository(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//}
--- src/main/java/com/oncheck/User.java
+++ src/main/java/com/oncheck/User.java
@@ -1,87 +1,89 @@
-package com.oncheck;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-
-@Entity
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="USER_ID")
-    private Long userId;
-
-    @Column(name="USERNAME")
-    private String username;
-
-    @Column(name="EMAIL")
-    private String email;
-
-    @Column(name="PASSWORD")
-    private String password;
-
-    @Column(name="LOGGEDIN")
-    private boolean loggedIn;
+//package com.oncheck;
 //
-//    @OneToMany
-//    @JoinColumn(name="RESTAURANT_ID")
-    private ArrayList<Restaurant> myEats;
-
-    public User(Long userID){
-        this.userId = userID;
-    }
-
-    public User() {};
-
-    public User(String username, String email, String password, boolean loggedIn){
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.loggedIn = loggedIn;
-        this.myEats = new ArrayList<Restaurant>();
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ArrayList<Restaurant> getMyEats() { return myEats; }
-
-    public void setMyEats(ArrayList<Restaurant> myEats) { this.myEats = myEats;}
-
-    public boolean isLoggedIn() {
-        return loggedIn;
-    }
-
-    public void setLoggedIn(boolean loggedIn) {
-        this.loggedIn = loggedIn;
-    }
-}
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//
+//@Entity
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name="USER_ID")
+//    private Long userId;
+//
+//    @Column(name="USERNAME")
+//    private String username;
+//
+//    @Column(name="EMAIL")
+//    private String email;
+//
+//    @Column(name="PASSWORD")
+//    private String password;
+//
+//    @Column(name="LOGGEDIN")
+//    private boolean loggedIn;
+////
+////    @OneToMany
+////    @JoinColumn(name="RESTAURANT_ID")
+//    private ArrayList<Restaurant> myEats;
+//
+//    public User(Long userID){
+//        this.userId = userID;
+//    }
+//
+//    public User() {};
+//
+//    public User(String username, String email, String password, boolean loggedIn){
+//        this.username = username;
+//        this.email = email;
+//        this.password = password;
+//        this.loggedIn = loggedIn;
+//        this.myEats = new ArrayList<Restaurant>();
+//    }
+//
+//    public Long getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(Long userId) {
+//        this.userId = userId;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public ArrayList<Restaurant> getMyEats() { return myEats; }
+//
+//    public void setMyEats(ArrayList<Restaurant> myEats) { this.myEats = myEats;}
+//
+//    public boolean isLoggedIn() {
+//        return loggedIn;
+//    }
+//
+//    public void setLoggedIn(boolean loggedIn) {
+//        this.loggedIn = loggedIn;
+//    }
+//
+//
+//}
